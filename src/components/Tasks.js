1a7ac3d42db80f63378dd2b6e2466e5d87b13f4e
Youssef Benali <youyssef@gmail.com>
2020-11-08 10:02:55 +0100

    nothing new

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,48 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
 import { Task } from './Task';
+import CustomButton from '../custom-components/CustomButton';
 
-const Tasks = ({ todos, filterDisplay, searchTerm, filter, visibleTodos }) => {
+const theme = {
+    bg_add: "#4fc08d",
+    bg_del: "#f03f3f"
+}
+
+const Tasks = ({ filterDisplay,todos, searchTerm, filter, visibleTodos }) => {
+    
+    useEffect(() => {
+        console.log("changes");
+    },[]);
+
     return (
-        <div className='tasks'>
-            {searchTerm.length < 1 ?
-                visibleTodos(todos, filter).map((todo, index) => <Task key={index} index={index} todo={todo} />)
-                : visibleTodos(filterDisplay, filter).map((todo, index) => <Task key={index} index={index} todo={todo} />)
+        <>
+            <div className='tasks'>
+                {searchTerm.length < 1 ?
+                    visibleTodos(todos, filter).map((todo, index) => <Task key={index} index={index} todo={todo} />)
+                    : visibleTodos(filterDisplay, filter).map((todo, index) => <Task key={index} index={index} todo={todo} />)
+                }
+            </div>
+            {/* <div className='tasks'>
+                {
+                    visibleTodos(filterDisplay, filter).map((todo, index) => <Task key={index} index={index} todo={todo} />)
+                }
+            </div> */}
+            {(filterDisplay.length > 0) ?
+                <div className='delete-state'>
+                    <CustomButton theme={{ bg: '#f03f3f' }}> Delete All </CustomButton>
+                </div> : <div className='empty-state'>
+                    <h2>Start your day by adding some tasks !</h2>
+                    <CustomButton theme={{ bg: '#4fc08d' }}> Add new Task </CustomButton>
+                </div>
             }
-        </div>
+            {/* {(todos.length === 0 || filterDisplay.lenght === 0) ?
+                <div className='empty-state'>
+                    <h2>Start your day by adding some tasks !</h2>
+                    <CustomButton theme={{ bg: '#4fc08d' }}> Add new Task </CustomButton>
+                </div> : null
+            } */}
+
+
+        </>
     )
 }
 
